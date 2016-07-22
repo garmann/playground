6d2eb3b0460895df153209b387b7b38270a5c40c
Gregor Armann <gregor.armann@googlemail.com>
2016-07-23 01:25:06 +0200

    improved packtpub parsing

--- python/packtpub_free_book/app.py
+++ python/packtpub_free_book/app.py
@@ -3,21 +3,54 @@
 import requests
 from bs4 import BeautifulSoup
 
-def main():
-    url = 'https://www.packtpub.com/packt/offers/free-learning'
 
+def soup_object(url):
+    """generates and returns soup object for given url
+
+    Args:
+        url - string: url of a website, here its packtpub
+    Returns:
+        soup - object: the soup object for later parsing
+    Raises:
+        Exception: if there is a problem with connection or parsing
+    """
     try:
         r = requests.get(url)
         soup = BeautifulSoup(r.content, "html.parser")
     except:
         raise Exception('problem with connection or parsing')
+    else:
+        return soup
 
-    # try block for basic book informations
+
+def parse_title(soup):
+    """parses title of soup object
+
+    Args:
+        soup - object: the soup object to parse
+    Returns:
+        title - string
+    Raises:
+        Exception: if soup cannot find the given div/class
+    """
     try:
-        # title 
         title = soup.find("div", class_="dotd-title").getText().strip()
+    except AttributeError:
+        raise Exception('problem with parsing')
+    else:
+        return title
 
-        # picture
+def parse_picture_url(soup):
+    """parses url of book image and some minor changes to the string
+
+    Args:
+        soup - object: the soup object to parse
+    Returns:
+        pic - string: the url of the book image
+    Raises:
+        Exception: if soup cannot find the given div/class
+    """
+    try:
         pic = soup.find("div", class_="dotd-main-book-image float-left").find("img")['src'].strip()
         pic = pic.replace(' ', '%20')
         if pic.startswith('//'):
@@ -25,10 +58,20 @@ def main():
     except AttributeError:
         raise Exception('problem with parsing')
     else:
-        print('Title:', title)
-        print('Image:', pic)
+        return pic
 
-    # try block for additional book informations
+
+def parse_more_info(soup, title):
+    """parses more informations about book
+
+    Args:
+        soup - object: the soup object to parse
+        title - string: string version of book title
+    Returns:
+        info - list: list of lines found with more informations
+    Raises:
+        raises none: if no informations are found, just continue
+    """
     try:
         info = []
         for i in soup.find("div", class_="dotd-main-book-summary float-left").find_all("div"):
@@ -36,13 +79,27 @@ def main():
             if 'Time is running out' not in ret \
                 and ret is not '' \
                 and title not in ret:
+
                 info.append(ret)
+
     except AttributeError:
         pass
     else:
-        print('Infos:...')
-        for x in info: print(x)
+        return info
+
+
+def main():
+    """handles site parsing of the packtpub site for free ebooks and prints all informations
+    """
+    url = 'https://www.packtpub.com/packt/offers/free-learning'
+    soup = soup_object(url)
+
+    print('title:', parse_title(soup))
+    print('imageurl:', parse_picture_url(soup))
+    print('additional informations:...')
+    for line in parse_more_info(soup, parse_title(soup)): print(line)
 
 
 if __name__ == '__main__':
     main()
+    
